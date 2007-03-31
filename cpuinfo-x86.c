a1df7bf079270a10bd1f6a3b87044ed8ae82e713
Gwenole Beauchesne <gb.public@free.fr>
2007-03-31 06:47:06 +0000

    Improve delay with a specific function (XXX move to -common.c).

--- cpuinfo-x86.c
+++ cpuinfo-x86.c
@@ -23,6 +23,7 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <assert.h>
+#include <errno.h>
 #include <sys/time.h>
 #include "cpuinfo.h"
 #include "cpuinfo-private.h"
@@ -307,6 +308,86 @@ static inline uint64_t get_ticks_usec(void)
   return ((uint64_t)tv.tv_sec * 1000000) + tv.tv_usec;
 }
 
+/*
+ *  Delay by specified number of microseconds (<1 second)
+ *  (adapted from SDL_Delay() source; this function is designed to provide
+ *  the highest accuracy possible)
+ */
+
+#if defined(__linux__)
+// Linux select() changes its timeout parameter upon return to contain
+// the remaining time. Most other unixen leave it unchanged or undefined.
+#define SELECT_SETS_REMAINING
+#elif defined(__FreeBSD__) || defined(__sun__) || (defined(__MACH__) && defined(__APPLE__))
+#define USE_NANOSLEEP
+#elif defined(HAVE_PTHREADS) && defined(sgi)
+// SGI pthreads has a bug when using pthreads+signals+nanosleep,
+// so instead of using nanosleep, wait on a CV which is never signalled.
+#include <pthread.h>
+#define USE_COND_TIMEDWAIT
+#endif
+
+static void delay_usec(uint32_t usec)
+{
+  int was_error;
+
+#if defined(USE_NANOSLEEP)
+  struct timespec elapsed, tv;
+#elif defined(USE_COND_TIMEDWAIT)
+  // Use a local mutex and cv, so threads remain independent
+  pthread_cond_t delay_cond = PTHREAD_COND_INITIALIZER;
+  pthread_mutex_t delay_mutex = PTHREAD_MUTEX_INITIALIZER;
+  struct timespec elapsed;
+  uint64_t future;
+#else
+  struct timeval tv;
+#ifndef SELECT_SETS_REMAINING
+  uint64_t then, now, elapsed;
+#endif
+#endif
+
+  // Set the timeout interval - Linux only needs to do this once
+#if defined(SELECT_SETS_REMAINING)
+  tv.tv_sec = 0;
+  tv.tv_usec = usec;
+#elif defined(USE_NANOSLEEP)
+  elapsed.tv_sec = 0;
+  elapsed.tv_nsec = usec * 1000;
+#elif defined(USE_COND_TIMEDWAIT)
+  future = GetTicks_usec() + usec;
+  elapsed.tv_sec = future / 1000000;
+  elapsed.tv_nsec = (future % 1000000) * 1000;
+#else
+  then = GetTicks_usec();
+#endif
+
+  do {
+	errno = 0;
+#if defined(USE_NANOSLEEP)
+	tv.tv_sec = elapsed.tv_sec;
+	tv.tv_nsec = elapsed.tv_nsec;
+	was_error = nanosleep(&tv, &elapsed);
+#elif defined(USE_COND_TIMEDWAIT)
+	was_error = pthread_mutex_lock(&delay_mutex);
+	was_error = pthread_cond_timedwait(&delay_cond, &delay_mutex, &elapsed);
+	was_error = pthread_mutex_unlock(&delay_mutex);
+#else
+#ifndef SELECT_SETS_REMAINING
+	// Calculate the time interval left (in case of interrupt)
+	now = GetTicks_usec();
+	elapsed = now - then;
+	then = now;
+	if (elapsed >= usec)
+	  break;
+	usec -= elapsed;
+	tv.tv_sec = 0;
+	tv.tv_usec = usec;
+#endif
+	was_error = select(0, NULL, NULL, NULL, &tv);
+#endif
+  } while (was_error && (errno == EINTR));
+}
+
 // Get processor frequency in MHz (x86info)
 int cpuinfo_get_frequency(void)
 {
@@ -322,7 +403,7 @@ int cpuinfo_get_frequency(void)
   start = get_ticks_usec();
   ticks_start = get_ticks();
   start = get_ticks_usec();
-  usleep(50000);
+  delay_usec(50000);
   ticks_stop = get_ticks();
   stop = get_ticks_usec();
   ticks_stop = get_ticks();
